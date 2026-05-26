a0f27088bcda8c3a4c8ace3b641f6ca73465f290
Sarah Borgelt <borgelt.sarah@gmail.com>
2026-05-26 19:02:52 -0400

    Updating spacing on expenses variable

--- expenses.py
+++ expenses.py
@@ -1,6 +1,6 @@
 # This exercise was completed as part of a training video with Sarah Holderness
 # in Python Foundations (Lists & Loops) on Plural Sight.
-expenses = [10.50, 8,5,15,20,5,3]
+expenses = [10.50, 8, 5, 15, 20, 5, 3]
 
 total = sum(expenses)
 
